52f6118473ee79f683f822cc9c4cb55cd72df766
Johnbosco <boscoseries@gmail.com>
2019-07-11 14:54:59 +0100

    setup joi for validation

---
+++ src/middleware/validate.ts
@@ -0,0 +1,38 @@
+import joi from "@hapi/joi";
+
+export const validateInput = (input: any) => {
+  const createSchema = {
+    firstname: joi
+      .string()
+      .min(2)
+      .required(),
+    surname: joi
+      .string()
+      .min(2)
+      .required(),
+    phone: joi
+      .string()
+      .min(11)
+      .required(),
+    mobile: joi
+      .string()
+      .min(11)
+      .optional(),
+    home: joi
+      .string()
+      .min(11)
+      .optional(),
+    email: joi
+      .string()
+      .email()
+      .optional(),
+    address: joi.string().optional(),
+    website: joi.string().optional(),
+    status: joi.string().optional()
+  };
+
+  const { error, value } = joi.validate(input, createSchema, {
+    abortEarly: false
+  });
+  return { error, value };
+};
