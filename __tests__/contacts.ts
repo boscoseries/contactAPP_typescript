b8c37bfd5793ef6c8fc9e3aa8cdcf7720e7a3ce0
Johnbosco <boscoseries@gmail.com>
2019-07-24 22:57:06 +0100

    added done function to tests

--- __tests__/contacts.ts
+++ __tests__/contacts.ts
@@ -31,7 +31,7 @@ describe("API Routes", () => {
       .send(contact2Data);
   };
 
-  test("POST /api/contacts should create a new contact", async () => {
+  test("POST /api/contacts should create a new contact", async (done) => {
     return await request(app)
       .post("/api/contacts")
       .send(contact1Data)
@@ -44,19 +44,21 @@ describe("API Routes", () => {
             status: "available"
           })
         );
+        done()
       });
   });
 
-  test("GET /api/contacts should return an array of 'not-blocked or deleted' contact", async () => {
+  test("GET /api/contacts should return an array of 'not-blocked or deleted' contact", async (done) => {
     return await request(app)
       .get("/api/contacts")
       .expect(res => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("contacts");
+        done();
       });
   });
 
-  test("GET /api/contacts/id should return a contact with the given ID", async () => {
+  test("GET /api/contacts/id should return a contact with the given ID", async (done) => {
     const contact = await contact2();
     const id = contact.body.data._id;
     return await request(app)
@@ -65,10 +67,11 @@ describe("API Routes", () => {
         expect(res.status).toBe(200);
         expect(res.body.data).toHaveProperty("_id", id);
         expect(res.body.data).toHaveProperty("email", "smartins@gmail.com");
+        done();
       });
   });
 
-  test("PATCH /api/contacts/id/details should return an updated contact", async () => {
+  test("PATCH /api/contacts/id/details should return an updated contact", async (done) => {
     const contact = await contact2();
     const id = contact.body.data._id;
     const details = {
@@ -83,10 +86,11 @@ describe("API Routes", () => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("data");
         expect(res.body.data).toHaveProperty("mobile", details.mobile);
+        done();
       });
   });
 
-  test("PATCH /api/contacts/id/status should return contact with an updated status", async () => {
+  test("PATCH /api/contacts/id/status should return contact with an updated status", async (done) => {
     const contact = await contact2();
     const id = contact.body.data._id;
     const details = {
@@ -99,10 +103,11 @@ describe("API Routes", () => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("data");
         expect(res.body.data).toHaveProperty("status", details.status);
+        done();
       });
   });
 
-  test("PATCH /api/contacts/id/delete should update contact's deleted status to true", async () => {
+  test("PATCH /api/contacts/id/delete should update contact's deleted status to true", async (done) => {
     const contact = await contact2();
     const id = contact.body.data._id;
     const details = {
@@ -115,10 +120,11 @@ describe("API Routes", () => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("data");
         expect(res.body.data).toHaveProperty("deleted", details.deleted);
+        done();
       });
   });
 
-  test("DELETE /api/contacts/id should DELETE contact completely from database", async () => {
+  test("DELETE /api/contacts/id should DELETE contact completely from database", async (done) => {
     const contact = await contact2();
     const id = contact.body.data._id;
     return await request(app)
@@ -127,6 +133,7 @@ describe("API Routes", () => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("message");
         expect(res.body.message).toContain(id);
+        done();
       });
   });
 });
--- __tests__/index.ts
+++ __tests__/index.ts
@@ -4,15 +4,16 @@ import mongoose from "mongoose";
 
 
 describe("API Routes", () => {
-  
+
   afterAll(async () => mongoose.connection.close());
-  test("GET / returns welcome onboard", async () => {
+  test("GET / returns welcome onboard", async (done) => {
     return await request(app)
       .get("/")
       .expect(res => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty("message");
         expect(res.body.message).toContain("welcome");
+        done();
       });
   });
 });
